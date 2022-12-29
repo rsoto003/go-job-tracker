e36dd206e0820fbabaea7a6505a439d54c80d997
Ryan Soto <ryansotodev@gmail.com>
2022-12-28 22:54:24 -0800

    hello world

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("we're going to build a job application tracker :) \n " +
+		"This project is going to be written in Go, with the Fiber framework for the front-end, and will use a Go backend with a PostgreSQL database.")
+}
